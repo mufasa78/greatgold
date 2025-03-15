0ed71ba14ad5915e7103e069f262503f9d4510dd
Mufasa <153092835+mufasa78@users.noreply.github.com>
2025-03-15 11:11:08 +0300

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import PaymentPage from './pages/PaymentPage';
+import ScrollToTop from './components/ScrollToTop';
+
+const Layout = ({ children }) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow">
+        {children}
+      </main>
+      <Footer />
+      <ScrollToTop />
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <HomePage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/payment/:productId"
+          element={
+            <Layout>
+              <PaymentPage />
+            </Layout>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App; 
